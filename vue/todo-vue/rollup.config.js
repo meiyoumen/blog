df823dfdcff2bf515fe7323c5ee72714416d4c0e
daysun <51134642@qq.com>
2019-08-02 19:16:34 +0800

    update

---
+++ vue/todo-vue/rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import vue from 'rollup-plugin-vue'
+import path from 'path'
+
+export default {
+  entry: path.resolve(__dirname, './app.js'),
+  dest: path.resolve(__dirname, './dist/app.js'),
+  format: 'iife',
+  moduleName: 'app',
+  sourceMap: true,
+  useStrict: false,
+  external: [
+    'vue',
+    'vue-router'
+  ],
+  plugins: [
+    vue(),
+    babel({
+      babelrc: false,
+      externalHelpers: false,
+      exclude: 'node_modules/**',
+      'plugins': [
+        ['transform-object-rest-spread', { 'useBuiltIns': true }]
+      ]
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs(),
+  ]
+}
